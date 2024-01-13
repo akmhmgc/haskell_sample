323fb47956daf4e41d8204b397f02dead9a5d1b3
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-13 17:44:08 +0900

    funciton

--- myfunctions.hs
+++ myfunctions.hs
@@ -8,3 +8,15 @@ doubleSmallNumber x =
     else x * 2
 
 doubleSmallNumber' x = doubleSmallNumber x + 1
+
+triples = [ (a,b,c) | c <- [1 .. 10], a <- [1 .. c], b <- [1 .. a], a^2 + b^2 == c^2]
+
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A' .. 'Z']]
+
+addOne :: Int -> Int
+addOne x = x
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
